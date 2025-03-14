6cf84646de828f14f77c2670053931b32abf07fd
ryanturnerwebdev <ryanturnerwebdev@gmail.com>
2025-03-14 11:25:44 +0000

    feat(docs): [WIP] add docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,109 +1,98 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'NetBox Documentation',
-  tagline: 'Centralised Documentation for NetBox',
-  favicon: 'img/favicon.ico',
-  url: 'https://docs.netbox.dev',
-  baseUrl: '/',
-  organizationName: 'netbox',
-  projectName: 'netbox-docs',
-  plugins: [
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: "netbox-docs",
-        path:"external-repos/netbox/docs",
-        routeBasePath: "docs/netbox",
-        sidebarPath: require.resolve('./sidebars.js'),
-      }
-    ],
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: "docusaurus",
-        path: "external-repos/github-pages-docusaurus/docs",
-        routeBasePath: "docs/docusaurus",
-        sidebarPath: require.resolve('./sidebars.js'),
-      }
-    ]
-  ],
+	title: "NetBox Documentation",
+	tagline: "Centralised Documentation for NetBox",
+	favicon: "img/favicon.ico",
+	url: "https://netboxlabs.com",
+	baseUrl: "/docs",
+	organizationName: "netbox",
+	projectName: "netbox-docs",
+	plugins: [
+		[
+			"@docusaurus/plugin-content-docs",
+			{
+				id: "netbox-docs",
+				path: "external-repos/netbox/docs",
+				routeBasePath: "netbox",
+				sidebarPath: require.resolve("./sidebars.js"),
+			},
+		],
+		[
+			"@docusaurus/plugin-content-docs",
+			{
+				id: "docusaurus",
+				path: "external-repos/github-pages-docusaurus/docs",
+				routeBasePath: "docusaurus",
+				sidebarPath: require.resolve("./sidebars.js"),
+			},
+		],
+	],
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-        },
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+				},
 
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'NetBox Documentation',
-      logo: {
-        alt: 'NetBox Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {to: 'docs/netbox', label: 'NetBox', position: 'left'},
-        {to: 'docs/docusaurus/intro', label: 'Docusaurus', position: 'left'},
-        {
-          href: 'https://github.com/netbox-community/netbox/',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "NetBox Documentation",
+			logo: {
+				alt: "NetBox Logo",
+				src: "img/logo.svg",
+			},
+			items: [
+				{ to: "netbox", label: "NetBox", position: "left" },
+				{ to: "docusaurus/intro", label: "Docusaurus", position: "left" },
+				{
+					href: "https://github.com/netbox-community/netbox/",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Community",
+					items: [
+						{
+							label: "X",
+							href: "https://x.com/NetBoxLabs",
+						},
+					],
+				},
+				{
+					title: "More",
+					items: [
+						{
+							label: "GitHub",
+							href: "https://github.com/netboxlabs",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} NetBox Labs.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
-
 
 export default config;
